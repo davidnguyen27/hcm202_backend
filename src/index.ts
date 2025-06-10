924e59cf7d65d20b635f53cb0134d0f920b5a895
davidnguyen27 <theanhngujen2702@gmail.com>
2025-06-10 14:26:52 +0700

    Fix Port

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.use(errorMiddleware)
 // DB + Server
 const PORT = process.env.PORT || 3000
 connectDB().then(() => {
-  app.listen(3000, () => {
+  app.listen(PORT, () => {
     console.log('Server is running at with port', PORT)
   })
 })
